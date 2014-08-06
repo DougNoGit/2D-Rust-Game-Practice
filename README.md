64cb3360ca09e3898db8282851f7c343aca3b948
DougNoGit <dougsmcgirr@gmail.com>
2014-08-05 21:27:58 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is my take on the PistonDev's "Getting Started" tutorial, which I built on a dare. It features the use of piston's keyboard support to move the square around. Take it. Hold it. Mod it. Love it. 
---
+++ src/game.rs
@@ -0,0 +1,116 @@
+extern crate graphics;
+extern crate piston;
+extern crate sdl2_game_window;
+extern crate opengl_graphics;
+
+use sdl2_game_window::GameWindowSDL2;
+use opengl_graphics::Gl;
+
+use piston::
+{
+    Game,
+    GameWindowSettings,
+    GameIteratorSettings,
+    RenderArgs,
+    UpdateArgs,
+    keyboard,
+    KeyPressArgs
+};
+
+use graphics::
+{
+    Context,
+    AddRectangle,
+    AddColor,
+    Draw,
+    RelativeTransform2d
+};
+
+static mut x : f64 = 100.0;
+static mut y : f64 = 0.0;
+
+pub struct App
+{
+    gl: Gl,
+    rotation: f64
+}
+
+impl Game for App
+{
+    fn render(&mut self, args: &RenderArgs)
+    {
+        let context = &Context::abs(args.width as f64, args.height as f64);
+        context.rgba(0.0,0.0,0.1,0.1).draw(&mut self.gl);
+
+        unsafe
+        {
+        context
+            .trans((args.width / 2) as f64, (args.height / 2) as f64)
+            .rot_rad(self.rotation)
+            .rect(x, y, 50.0, 50.0)
+            .rgba(0.0, 0.5, 1.0, 1.0)
+            .trans(-25.0, -25.0)
+            .draw(&mut self.gl)
+        }
+    }
+
+    fn update(&mut self, args: &UpdateArgs) 
+    {
+        self.rotation += 0.0 * args.dt; // Change this value to a non-zero number to make the sprite rotate.
+    }
+
+    fn key_press(&mut self, _args: &KeyPressArgs)
+    {
+        if _args.key == keyboard::Up
+        {
+            unsafe
+            {
+                y -= 10.0;
+            }
+        }
+        if _args.key == keyboard::Down
+        {
+            unsafe
+            {
+                y += 10.0;
+            }
+        }
+        if _args.key == keyboard::Left
+        {
+            unsafe
+            {
+                x -= 10.0;
+            }
+        }
+        if _args.key == keyboard::Right
+        {
+            unsafe
+            {
+                x += 10.0;
+            }
+        }
+    }
+
+}
+
+fn main()
+{
+    let mut window = GameWindowSDL2::new(
+        GameWindowSettings
+        {
+            title: "YOLO".to_string(),
+            size: [800, 800],
+            fullscreen: false,
+            exit_on_esc: true
+        }
+);
+
+let game_iter_settings = GameIteratorSettings
+{
+    updates_per_second: 60,
+    max_frames_per_second: 60
+};
+
+let mut app = App { gl: Gl::new(), rotation: 0.0 };
+app.run(&mut window, &game_iter_settings);
+}
